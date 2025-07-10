f9c13eae953c390b9b5b9376b6b1c8bc246f9c17
iceanddust <iceanddust@proton.me>
2025-07-10 18:58:36 -0300

    Add useBatchUpgradeAndSavingsSupply hook for batch upgrading and supplying savings

---
+++ packages/hooks/src/savings/useBatchUpgradeAndSavingsSupply.ts
@@ -0,0 +1,95 @@
+import { useAccount, useChainId } from 'wagmi';
+import { BatchWriteHook, BatchWriteHookParams } from '../hooks';
+import { useSavingsAllowance } from './useSavingsAllowance';
+import { sUsdsAddress, sUsdsImplementationAbi } from './useReadSavingsUsds';
+import { getWriteContractCall } from '../shared/getWriteContractCall';
+import { daiUsdsAbi, daiUsdsAddress, mcdDaiAddress, usdsAddress } from '../generated';
+import { Call, erc20Abi } from 'viem';
+import { useSendBatchTransactionFlow } from '../shared/useSendBatchTransactionFlow';
+import { useTokenAllowance } from '../tokens/useTokenAllowance';
+
+export function useBatchUpgradeAndSavingsSupply({
+  amount,
+  onSuccess = () => null,
+  onError = () => null,
+  onStart = () => null,
+  enabled: paramEnabled = true,
+  ref = 0
+}: BatchWriteHookParams & {
+  amount: bigint;
+  ref?: number;
+}): BatchWriteHook {
+  const { address: connectedAddress, isConnected } = useAccount();
+  const chainId = useChainId();
+
+  const { data: daiUgradeAllowance } = useTokenAllowance({
+    chainId,
+    contractAddress: mcdDaiAddress[chainId as keyof typeof mcdDaiAddress],
+    owner: connectedAddress,
+    spender: daiUsdsAddress[chainId as keyof typeof daiUsdsAddress]
+  });
+  const { data: usdsSupplyAllowance, error: allowanceError } = useSavingsAllowance();
+
+  const hasDaiUpgradeAllowance = daiUgradeAllowance !== undefined && daiUgradeAllowance >= amount;
+  const hasUsdsSupplyAllowance = usdsSupplyAllowance !== undefined && usdsSupplyAllowance >= amount;
+
+  // Calls for the batch transaction
+  const calls: Call[] = [];
+
+  const approveDaiCall = getWriteContractCall({
+    to: mcdDaiAddress[chainId as keyof typeof mcdDaiAddress],
+    abi: erc20Abi,
+    functionName: 'approve',
+    args: [daiUsdsAddress[chainId as keyof typeof daiUsdsAddress], amount]
+  });
+
+  const upgradeCall = getWriteContractCall({
+    to: daiUsdsAddress[chainId as keyof typeof daiUsdsAddress],
+    abi: daiUsdsAbi,
+    functionName: 'daiToUsds',
+    args: [connectedAddress!, amount]
+  });
+
+  if (!hasDaiUpgradeAllowance) calls.push(approveDaiCall);
+  calls.push(upgradeCall);
+
+  const approveUsdsCall = getWriteContractCall({
+    to: usdsAddress[chainId as keyof typeof usdsAddress],
+    abi: erc20Abi,
+    functionName: 'approve',
+    args: [sUsdsAddress[chainId as keyof typeof sUsdsAddress], amount]
+  });
+
+  const supplyCall = getWriteContractCall({
+    to: sUsdsAddress[chainId as keyof typeof sUsdsAddress],
+    abi: sUsdsImplementationAbi,
+    functionName: 'deposit',
+    args: [amount, connectedAddress!, ref]
+  });
+
+  if (!hasUsdsSupplyAllowance) calls.push(approveUsdsCall);
+  calls.push(supplyCall);
+
+  const enabled =
+    isConnected &&
+    !!amount &&
+    amount !== 0n &&
+    hasDaiUpgradeAllowance !== undefined &&
+    hasUsdsSupplyAllowance !== undefined &&
+    paramEnabled &&
+    !!connectedAddress;
+
+  const sendBatchTransactionFlowResults = useSendBatchTransactionFlow({
+    calls,
+    chainId,
+    enabled,
+    onSuccess,
+    onError,
+    onStart
+  });
+
+  return {
+    ...sendBatchTransactionFlowResults,
+    error: sendBatchTransactionFlowResults.error || allowanceError
+  };
+}
