cf6a2c66f7c14c975c7876a42f90547f5c42c0a6
inclinedmind <138821869+inclinedmind@users.noreply.github.com>
2026-04-14 15:52:17 -0700

    Revert "Merge pull request #1485 from jetstreamgg/hotfix/remove-cow-external-link"
    This reverts commit 8d31991b1f5738414cd3bbdbf01d5d08020c128d, reversing
    changes made to d029cae55a86df6c9f5766314053e1dc5c846171.

--- packages/widgets/src/widgets/TradeWidget/components/TradeHeader.tsx
+++ packages/widgets/src/widgets/TradeWidget/components/TradeHeader.tsx
@@ -2,6 +2,7 @@ import { Trans } from '@lingui/react/macro';
 import { Dispatch, SetStateAction } from 'react';
 import { TradeConfigMenu } from './TradeConfigMenu';
 import { Heading, Text } from '@widgets/shared/components/ui/Typography';
+import { ExternalLink } from '@widgets/shared/components/ExternalLink';
 import { CoW } from '@widgets/shared/components/icons/CoW';
 
 type PropTypes = {
@@ -10,6 +11,7 @@ type PropTypes = {
   isEthFlow?: boolean;
   ttl: string;
   setTtl: Dispatch<SetStateAction<string>>;
+  onExternalLinkClicked?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
 };
 
 export const TradeHeader = ({
@@ -18,7 +20,7 @@ export const TradeHeader = ({
   isEthFlow = false,
   ttl,
   setTtl
-}: PropTypes): React.ReactElement => {
+}: Omit<PropTypes, 'originToken' | 'onExternalLinkClicked'>): React.ReactElement => {
   return (
     <div className="flex items-baseline justify-between gap-2">
       <Heading variant="x-large">
@@ -41,10 +43,24 @@ export const TradeSubHeader = () => (
   </Text>
 );
 
-export const TradePoweredBy = () => (
+export const TradePoweredBy = ({
+  onExternalLinkClicked
+}: {
+  onExternalLinkClicked?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+}) => (
   <div className="mb-4 flex items-center gap-1.5">
-    <Text className="text-text text-sm leading-none font-normal">Powered by CoW Protocol</Text>
+    <Text className="text-text text-sm leading-none font-normal">
+      Powered by{' '}
+      <ExternalLink
+        href="https://cow.fi/"
+        showIcon={true}
+        iconSize={12}
+        wrapperClassName="gap-1"
+        onExternalLinkClicked={onExternalLinkClicked}
+      >
+        CoW Protocol
+      </ExternalLink>
+    </Text>
     <CoW className="rounded-[0.25rem]" />
   </div>
 );
-
